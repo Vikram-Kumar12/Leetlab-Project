9517db75406fc20962a31ba2e48f718a3ad28130
vikram kumar <vikramkumar0120arav@gmail.com>
2025-04-21 14:18:16 +0530

    First Day : 21-04-2025 . 🚀 Setup backend structure with dotenv, app.js, and basic server using app.listen

---
+++ Backend/src/index.js
@@ -0,0 +1,12 @@
+import app from "./App.js"
+ import dotenv from 'dotenv';
+ 
+ dotenv.config({
+     path:"./.env"
+ })
+ const PORT = process.env.PORT || 4000;
+ 
+ 
+ app.listen(PORT,()=>{
+     console.log(`Server is started on port: ${PORT}`);
+ })
